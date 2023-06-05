a045882be14af049bd8e1c4a2aec5b1756d5e483
nicolozibra1 <nico.zibra@gmail.com>
2023-06-05 13:06:24 +0200

    fix show.blade - body html printed

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,5 +3,5 @@
 @section('content')
     <h1>{{ $post->title }}</h1>
     <img src="{{ $post->image }}" alt="{{ $post->title }}">
-    <p>{{ $post->body }}</p>
+    <p>{!! $post->body !!}</p>
 @endsection
