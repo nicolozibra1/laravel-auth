c04248f335e7063926e68ececf3868b03a79457a
nicolozibra1 <nico.zibra@gmail.com>
2023-06-05 17:48:55 +0200

    upgrade index Projects & added first project

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(3);
         return view('admin.projects.index', compact('projects'));
     }
 
