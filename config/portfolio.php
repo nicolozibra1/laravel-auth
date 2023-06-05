33843a8a51f88ad06e3ae7ccc1b86b632967ad69
nicolozibra1 <nico.zibra@gmail.com>
2023-06-05 16:01:48 +0200

    create-Projects (Model, Migration and Seeder)

---
+++ config/portfolio.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'projects' => [
+        [
+            "title" => "Discord Remake",
+            "image" => "percorso del immagine",
+            "description" => "Questo progetto consiste nella realizzazione di un Remake Front-End del famoso Discord.",
+            "functionality" => "Front-End Graphic Remake",
+            "technologies" => "HTML, CSS, Bootstrap",
+            "resource" => "link del progetto su Github"
+        ],
+    ]
+];
